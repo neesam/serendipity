0f8462af6fd9a7d90cd3084291ff58cc841dd4bf
Andy Nees <neesam@mymail.vcu.edu>
2025-08-28 23:28:37 -0600

    add to stremio endpoint

--- Backend/controllers/stremio.ts
+++ Backend/controllers/stremio.ts
@@ -5,41 +5,40 @@ import { Request, Response } from "express";
 
 import { bigquery } from "../utils/bigQuery";
 
-import { appendFile, unlink } from "fs";
-
 const FILM_TABLES_DATASET = process.env.FILM_TABLES_DATASET;
 const BQ_PROJECT = process.env.BQ_PROJECT;
 
 const stremioLogic = async (req: Request, res: Response) => {
     try {
-        unlink("stremio.txt", (err) => {
-            console.log("Successfully deleted stremio.txt!");
-        });
-
-        const date = new Date();
-
-        let query = `SELECT * FROM ${BQ_PROJECT}.${FILM_TABLES_DATASET}.film_stremiolibrary WHERE date_last_accessed IS NULL`;
-
-        console.log(query);
+        let query = `DELETE FROM ${BQ_PROJECT}.${FILM_TABLES_DATASET}.film_addtostremio WHERE TRUE`;
 
         let [job] = await bigquery.createQueryJob(query);
 
         let [rows] = await job.getQueryResults();
 
+        const date = new Date();
+
+        query = `SELECT * FROM ${BQ_PROJECT}.${FILM_TABLES_DATASET}.film_stremiolibrary WHERE date_last_accessed IS NULL`;
+
+        console.log(query);
+
+        [job] = await bigquery.createQueryJob(query);
+
+        [rows] = await job.getQueryResults();
+
         console.log(rows);
 
         for (let i = 0; i < rows.length; i++) {
-            appendFile("stremio.txt", rows[i]["title"], (err) => {
-                if (err) {
-                    console.error("Error appending to file:", err);
-                    return;
-                }
-                console.log(
-                    `${rows[i]["title"]} appended to stremio.txt successfully!`
-                );
-            });
+            let title = rows[i]["title"];
+            query = `INSERT INTO ${BQ_PROJECT}.${FILM_TABLES_DATASET}.film_addtostremio (title) values ('${rows[i]["title"]}')`;
 
-            res.json(`Added ${rows[i]["title"]}  to stremio.txt!`);
+            console.log(query);
+
+            [job] = await bigquery.createQueryJob(query);
+
+            [rows] = await job.getQueryResults();
+
+            res.json(`Added ${title} to film_addtostremio!`);
         }
 
         query = `UPDATE ${BQ_PROJECT}.${FILM_TABLES_DATASET}.film_stremiolibrary SET date_last_accessed = '${date.toDateString()}' WHERE TRUE`;
@@ -47,6 +46,8 @@ const stremioLogic = async (req: Request, res: Response) => {
         [job] = await bigquery.createQueryJob(query);
 
         [rows] = await job.getQueryResults();
+
+        res.json(rows);
     } catch (error) {
         if (error instanceof Error) {
             console.log(error.message);
